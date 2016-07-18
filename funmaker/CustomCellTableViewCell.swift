ddbec0e747c222574e97a3a23f3b81aa6264d55d
wxyora@sina.com <wxyora@sina.com>
2016-07-19 00:30:38 +0800

    111

---
+++ funmaker/CustomCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomCellTableViewCell.swift
+//  funmaker
+//
+//  Created by Waylon on 16/7/18.
+//  Copyright © 2016年 Waylon. All rights reserved.
+//
+
+import UIKit
+
+class CustomCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
