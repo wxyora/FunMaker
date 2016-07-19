ce8e286d807cd1af0ae83a4111361c8e41d183df
waylon <wxyora@sina.com>
2016-07-19 17:52:56 +0800

    111

--- funmaker/IndexViewController.swift
+++ funmaker/IndexViewController.swift
@@ -5,14 +5,19 @@ class IndexViewController: UITableViewController,UISearchBarDelegate{
     @IBOutlet weak var myWebView: UIWebView!
     @IBOutlet weak var progressShow: UIActivityIndicatorView?
     
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var pageScrollView: UIScrollView!
+    
+  
+    
     @IBOutlet weak var searchBar: UISearchBar!
     
-     var str = ["精装修别墅","豪华洋房","三亚海景房","重庆江景房"]
+     var str = ["精装修别墅","豪华洋房","三亚海景房","重庆江景房","精装修别墅","豪华洋房","三亚海景房","重庆江景房"]
    // @IBOutlet var searchBar: UISearchBar!
     
-    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-        self.searchBar.resignFirstResponder()
-    }
+//    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+//        self.searchBar.resignFirstResponder()
+//    }
  
     override func viewDidLoad() {
         
@@ -28,13 +33,48 @@ class IndexViewController: UITableViewController,UISearchBarDelegate{
         
          //self.tableView.registerClass(CustomCell.self, forCellReuseIdentifier:"RentInfoCell")
         
-        self.searchBar.delegate = self
+       // self.searchBar.delegate = self
         
         let rc = UIRefreshControl()
         rc.attributedTitle = NSAttributedString(string: "下拉刷新")
         rc.addTarget(self, action: #selector(IndexViewController.refreshTableView), forControlEvents: UIControlEvents.ValueChanged)
         self.refreshControl = rc
         
+        //set pagecontrol infomation
+        //pageControl.currentPage = 0
+        //pageControl.numberOfPages = 4
+        
+        // 获取scrollview wight and hight
+        let wight:CGFloat=320
+        let hight=self.pageScrollView.frame.height
+        //设置scrollview 4个区域
+        pageScrollView.contentSize = CGSize(width: 4*wight, height: hight)
+        //set scrollview delegete
+        pageScrollView.delegate=self
+        
+        let totalCount = 4
+        
+     
+        
+        for index in 0..<totalCount{
+            //let imageView:UIImageView = UIImageView();
+            let imageX:CGFloat = CGFloat(index) * wight;
+            let imageView:UIImageView = UIImageView(frame: CGRectMake(imageX, 0, 320, 128))
+            //let imageX:CGFloat = CGFloat(index) * wight;
+            //imageView.frame = CGRectMake(imageX, hight, wight, hight);//设置图片的大小，注意Image和ScrollView的关系，其实几张图片是按顺序从左向右依次放置在ScrollView中的，但是ScrollView在界面中显示的只是一张图片的大小，效果类似与画廊；
+            let name:String = String(format: "gallery%d.png", index);
+            //imageView.image = UIImage(named:name);
+            imageView.image = UIImage(named: name)
+            self.pageScrollView.showsHorizontalScrollIndicator = false;//不设置水平滚动条；
+            self.pageScrollView.addSubview(imageView)//把图片加入到ScrollView中去，实现轮播的效果；
+            //self.pageScrollView.addSubview(imageView)
+        }
+        //通过坐标和大小来创建图像视图
+//                var imageView:UIImageView = UIImageView(frame: CGRectMake(0, 0, 320, 128))
+//                imageView.image = UIImage(named: "gallery1.png")
+//                self.pageScrollView.addSubview(imageView)
+        
+    
     }
     
 
