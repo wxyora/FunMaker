6999c6934b3bf86f1c55a5829b75ce8425b2143c
wxyora@sina.com <wxyora@sina.com>
2016-08-15 20:38:06 +0800

    111

--- funmaker/funmaker-Bridging-Header.h
+++ funmaker/funmaker-Bridging-Header.h
@@ -6,9 +6,16 @@
 #import "MBProgressHUD.h"
 #import <ShareSDK/ShareSDK.h>
 
+#import <ShareSDKConnector/ShareSDKConnector.h>
 
-//#import <TencentOpenAPI/QQApiInterface.h>
-//#import <TencentOpenAPI/TencentOAuth.h>
-//#import "WXApi.h"
-//#import "WeiboSDK.h"
-//#import <RennSDK/RennSDK.h>
+//腾讯开放平台（对应QQ和QQ空间）SDK头文件
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/QQApiInterface.h>
+
+//微信SDK头文件
+#import "WXApi.h"
+
+//新浪微博SDK头文件
+#import "WeiboSDK.h"
+//新浪微博SDK需要在项目Build Settings中的Other Linker Flags添加"-ObjC"
+
